32e9605fddaaa8b874ef5002a507cd53cc8f51a4
harry fitzgerald <hfitz83@gmail.com>
2019-02-11 12:39:39 +0000

    added editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-	mode: 'development',
-	entry: './lib/game.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'game.bundle.js'
-	},
-	devtool: 'source-map'
-}
+  mode: 'development',
+  entry: './lib/game.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'game.bundle.js',
+  },
+  devtool: 'source-map',
+};
+
