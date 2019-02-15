84311894e773bd067e9cb8ed82c03b442950ba93
harry fitzgerald <hfitz83@gmail.com>
2019-02-15 10:57:25 +0000

    move to require statements

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const http = require('http')
+const http = require('http');
 
 const app = express();
-const port = process.env.PORT || 3000
-const server = http.createServer(app)
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 
-const io = require('socket.io')(server)
-const p2p = require('socket.io-p2p-server').Server
-io.use(p2p)
+const io = require('socket.io')(server);
+const p2p = require('socket.io-p2p-server').Server;
 
-let players = []
-let readyCount = 0
+io.use(p2p);
 
-io.on('connection', function (socket) {
-  console.log('connected', socket.id)
-  players.push({id:socket.id, isReady:false})
+const players = [];
+let readyCount = 0;
 
-  console.log('all players', players)
+io.on('connection', (socket) => {
+  console.log('connected', socket.id);
+  players.push({ id: socket.id, isReady: false });
 
-  console.log('sending players to all other clients')
-  io.emit('players',players)
+  console.log('all players', players);
 
-  socket.on('ready-up', function (data) {
-    console.log(`${socket.id} is ready`)
-    readyCount++
+  console.log('sending players to all other clients');
+  io.emit('players', players);
 
-    console.log(readyCount, io.engine.clientsCount)
+  socket.on('ready-up', (data) => {
+    console.log(`${socket.id} is ready`);
+    readyCount++;
+
+    console.log(readyCount, io.engine.clientsCount);
 
     if (readyCount === io.engine.clientsCount) {
-      console.log('seems everyone is ready, lets roll')
-      io.emit('go-private')
+      console.log('seems everyone is ready, lets roll');
+      io.emit('go-private');
     }
 
     // players.map((p) => {
@@ -43,21 +44,21 @@ io.on('connection', function (socket) {
     //
     //
     // })
-  })
+  });
 
   socket.on('disconnect', () => {
-    console.log('disconnected', socket.id)
+    console.log('disconnected', socket.id);
     players.forEach((player, index) => {
       if (socket.id === player.id) {
         players.splice(index, 1);
-        readyCount = 0
+        readyCount = 0;
       }
-    })
-  })
-})
+    });
+  });
+});
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-server.listen(port, () => console.log(`server listening on ${port}`))
+server.listen(port, () => console.log(`server listening on ${port}`));
